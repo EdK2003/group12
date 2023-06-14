957375768cdd6f81e10773cad9d83aaf792b2385
annaschouten <118740214+annaschouten@users.noreply.github.com>
2023-06-14 10:04:56 +0200

    Update model_knn.py

--- model_knn.py
+++ model_knn.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 
-df = pd.read_csv(r"C:\Users\20202296\OneDrive - TU Eindhoven\Documents\Year 3\Advanced Programming\mol_desc_given.csv", index_col=0)
+df = pd.read_csv("mol_desc_given.csv", index_col=0)
 
 X = df[set(df.columns) - set(['ALDH1_inhibition'])]
 y = df['ALDH1_inhibition']
