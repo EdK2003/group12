fd210aa1eadb5ceb13a2defb2f608d5cb33cde5e
annaschouten <118740214+annaschouten@users.noreply.github.com>
2023-06-14 10:04:28 +0200

    Add files via upload

---
+++ model_knn.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+
+df = pd.read_csv(r"C:\Users\20202296\OneDrive - TU Eindhoven\Documents\Year 3\Advanced Programming\mol_desc_given.csv", index_col=0)
+
+X = df[set(df.columns) - set(['ALDH1_inhibition'])]
+y = df['ALDH1_inhibition']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+knn = KNeighborsClassifier(n_neighbors=3)
+
+knn.fit(X_train,y_train)
