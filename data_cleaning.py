29f890179b168a6122d87dcad785b4a2923dda7d
annaschouten <118740214+annaschouten@users.noreply.github.com>
2023-06-16 21:11:20 +0200

    Update data_cleaning.py

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,13 +1,28 @@
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
+import numpy as np
 
-scaler = MinMaxScaler()
-
+# read data from file
 df_data_raw = pd.read_csv('C:\\Git files\\group12\\mol_desc_given.csv', index_col='SMILES')
+
+# scale data
+scaler = MinMaxScaler()
 header = df_data_raw.columns
 index = df_data_raw.index
 df_data_scaled = pd.DataFrame(scaler.fit_transform(df_data_raw), index=index, columns=header)
 
 df_corr = df_data_scaled.loc[:,'MaxAbsEStateIndex':'fr_urea'].corr()
 
-print()
+# drop highly correlated variables
+upper_tri = df_corr.where(np.triu(np.ones(df_corr.shape),k=1).astype(bool))
+to_drop = [column for column in upper_tri.columns if any(upper_tri[column] > 0.95)]
+df_data_scaled.drop(to_drop, axis=1, inplace=True)
+
+# drop columns with missing data
+missing_data = df_data_scaled.isnull()
+for col in missing_data:
+    if True in missing_data[col].unique():
+        df_data_scaled.drop(col, axis=1, inplace=True)
+        
+# rop duplicate rows
+df_data_scaled.drop_duplicates(inplace=True)
